6bc86f641d2e58f235773956c6733331bf734c1e
Alex Livhsiz <alex.livshiz>
2018-07-18 12:39:28 +0300

    I have a map with markers

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
   title: string = 'My first AGM project';
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+  lat: number = 32;
+  lng: number = 34;
 
 
   constructor() { }
